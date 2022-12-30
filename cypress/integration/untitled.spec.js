0ba77863ab00bdc9ee083e8da90dd54281af3999
Thai Huu Luong <102707239+THAIHUULUONG@users.noreply.github.com>
2022-12-30 09:12:05 +0700

    crypress

---
+++ cypress/integration/untitled.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('https://dapp.bami.money')
+    })
+  
+    // https://on.cypress.io/interacting-with-elements
+  
+    it('.type() - type into a DOM element', () => {
+      // https://on.cypress.io/type
+      cy.visit('https://dapp.bami.money')
+    })
+  })
+  
